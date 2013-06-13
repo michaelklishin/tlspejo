512c1f3d9ca3d70f257eefb07ca0651b4d9cc7ee
Michael Klishin <michael@defprotocol.org>
2013-06-13 16:09:06 +0400

    Be friendly to Wireshark
    Switch to IPv4, explicitly use ciphers Wireshark supports.

--- main.go
+++ main.go
@@ -13,17 +13,21 @@ import (
 var keyFile  = flag.String("key-file", "./tls/server/key.pem", "Server private key to use")
 var certFile = flag.String("cert-file", "./tls/server/cert.pem", "Server certificate to use")
 
+var cipherSuites = []uint16{tls.TLS_RSA_WITH_RC4_128_SHA, tls.TLS_RSA_WITH_AES_128_CBC_SHA, tls.TLS_RSA_WITH_AES_256_CBC_SHA}
+
 func main() {
         flag.Parse()
 
-        endpoint := ":4433"
+        endpoint := "127.0.0.1:14433"
         cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
 
         if err != nil {
                 log.Fatalf("Failed to load key or cert: %s", err)
         }
         config := tls.Config{Certificates: []tls.Certificate{cert}, ClientAuth: tls.RequireAnyClientCert}
-        config.Rand = rand.Reader        
+        config.Rand = rand.Reader
+        // Wireshark supports these
+        config.CipherSuites = cipherSuites
 
         bind(endpoint, &config)
 }
@@ -63,21 +67,28 @@ func serve(conn net.Conn) {
         }
 
         state := tlscon.ConnectionState()
+
         log.Println("Client public key is:")
         for _, v := range state.PeerCertificates {
             log.Print(v.PublicKey)
         }
+
+        log.Printf("Negotiated protocol: %s, mutual?: %t\n", state.NegotiatedProtocol, state.NegotiatedProtocolIsMutual)
 
         var buf [4096]byte
 
         for {
                 n, err := conn.Read(buf[0:])
                 if err != nil {
+                        log.Printf("Done serving %s\n", conn.RemoteAddr().String())
                         return
                 }
+                s := string(buf[0:])
+                log.Printf("Received: %s\n", s)
 
                 _, err2 := conn.Write(buf[0:n])
                 if err2 != nil {
+                        log.Printf("Done serving %s\n", conn.RemoteAddr().String())
                         return
                 }
         }
